8ea4923037b341a4fa00d73bb26577ae17a808fa
root <root@ip-10-0-0-47.ec2.internal>
2021-04-18 22:21:38 +0000

    cambios en el bash_minikube

--- bash_minikube.sh
+++ bash_minikube.sh
@@ -3,22 +3,26 @@
 
 #una vez dentro de la instancia: 
 
-sudo su
+echo 'ejecucion de script para levantar minikube'
 
+#sudo su
+
+cd ..
 cd /root && minikube start --driver=none
 
 cd /opt/kf-educate && /root/kfctl apply -V -f kfctl_k8s_istio.v1.0.2.yaml
 
 ##//esperara tres minutos 
 
-## sleep
+echo 'cargando servicios, me echo una siesta'
+
 sleep 3m
 
+echo 'creando minikube con jupypter lab y ant colony '
 ## Crea nuestro minikube 
 OPT_URL=https://raw.githubusercontent.com/optimizacion-2-2021-1-gh-classroom/practica-2-primera-parte-ltejadal/main/deployments/minikube
 OPT_PV=hostpath-pv
 kubectl create -f $OPT_URL/hostpath_pv/$OPT_PV.yaml
-
 
 kubectl create -f https://raw.githubusercontent.com/optimizacion-2-2021-1-gh-classroom/practica-2-primera-parte-ltejadal/main/deployments/minikube/hostpath_pv/hostpath-pvc.yaml
 
@@ -27,3 +31,6 @@ kubectl create -f https://raw.githubusercontent.com/ITAM-DS/analisis-numerico-co
 
 kubectl create -f https://raw.githubusercontent.com/optimizacion-2-2021-1-gh-classroom/practica-2-primera-parte-ltejadal/main/deployments/minikube/hostpath_pv/jupyterlab-opt-0.1-hostpath-pv.yaml
 
+echo 'servicios iniciados correctamente carnal'
+
+kubectl get pods -n kubeflow
